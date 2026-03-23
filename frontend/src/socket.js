c87288954273460258a481634a8ac34fdc6f74f2
Akashlodhi <127076693+Akashlodhi928@users.noreply.github.com>
2026-03-23 15:30:01 +0530

    Change socket connection URL to production

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client"
 
-const socket = io("http://localhost:8000", {
+const socket = io("https://attendance-system-j9d3.onrender.com", {
   transports: ["websocket"],
   autoConnect: true,
   withCredentials: true
