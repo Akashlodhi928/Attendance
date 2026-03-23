de3cfb647e6afe63fa7815869cd4297d2923d18c
Akashlodhi <127076693+Akashlodhi928@users.noreply.github.com>
2026-03-23 15:29:41 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 
-export const serverUrl = "http://localhost:8000"
+export const serverUrl = "https://attendance-system-j9d3.onrender.com"
 createRoot(document.getElementById('root')).render(
   <AuthProvider>
     <App />
