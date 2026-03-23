f3440476370af889dd5de186cf748d2c78fa7d3c
Akashlodhi <127076693+Akashlodhi928@users.noreply.github.com>
2026-03-23 18:45:40 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken"
 
 export const isAuth = async (req, res , next) => {
  try {
-  const token = req.headers.authorization?.split(" ")[1]
+ let token = req.cookies.token
 
 if (!token) {
   return res.status(401).json({ message: "Unauthorized" })
