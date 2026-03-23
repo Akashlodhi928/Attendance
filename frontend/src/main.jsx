ed61d465eb4916788b27e01105a8b90071f69bff
Akashlodhi <127076693+Akashlodhi928@users.noreply.github.com>
2026-03-23 17:27:51 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
+import axios from "axios"
+
+// 🔥 IMPORTANT LINE (MISSING THA)
+axios.defaults.withCredentials = true
 
 export const serverUrl = "https://attendance-system-j9d3.onrender.com"
+
 createRoot(document.getElementById('root')).render(
   <AuthProvider>
     <App />
