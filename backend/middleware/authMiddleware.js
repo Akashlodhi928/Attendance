ed23305bbbee0effc32bdfecaa1d583fecff3597
Akashlodhi <127076693+Akashlodhi928@users.noreply.github.com>
2026-03-23 18:34:48 +0530

    Refactor token retrieval in auth middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,11 +2,11 @@ import jwt from "jsonwebtoken"
 
 export const isAuth = async (req, res , next) => {
  try {
-    let token = req.cookies.token
+  const token = req.headers.authorization?.split(" ")[1]
 
-    if(!token){
-        return res.status(401).json({message:"Unauthorized, token not found"})
-    }
+if (!token) {
+  return res.status(401).json({ message: "Unauthorized" })
+}
 
     const decoded = jwt.verify(token , process.env.JWT_SECRET)
     req.user = decoded
