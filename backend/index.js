891fe053a16b419e8f782c0c04ac84c99d2b898b
Akashlodhi <127076693+Akashlodhi928@users.noreply.github.com>
2026-03-23 15:35:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ const server = http.createServer(app);
 /* 🔥 SOCKET SETUP */
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://attendance-564p.onrender.com",
     credentials: true
   }
 });
@@ -40,7 +40,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://attendance-564p.onrender.com"],
     credentials: true,
   })
 );
